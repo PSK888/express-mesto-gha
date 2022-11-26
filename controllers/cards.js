1e559402242ad46a0d32136209b822bf04b82fe7
Pavel Krupin <pskrupin@yandex.ru>
2022-11-26 12:02:19 +0300

    13projectwork after third rev

--- controllers/cards.js
+++ controllers/cards.js
@@ -31,7 +31,9 @@ const deleteCardById = (req, res) => {
     .orFail(() => new Error('NotFound'))
     .then(() => res.status(STATUS_OK).send({ message: 'Карточка удалена.' }))
     .catch((err) => {
-      if (err.message === 'NotFound') {
+      if (err.name === 'CastError') {
+        return res.status(STATUS_BAD_REQUEST).send({ message: 'Карточка с указанным id не найдена.' });
+      } if (err.message === 'NotFound') {
         return res.status(STATUS_NOT_FOUND).send({ message: 'Карточка не найдена' });
       }
       return res.status(STATUS_INTERNAL_SERVER_ERROR).send({ message: 'На сервере произошла ошибка.' });
